e6b16d294543a01fd358b6feae53495ed6903f47
Will <willblev@gmail.com>
2017-04-11 19:49:37 +0200

    Update Wills_UDS_ATC.ino
    added PID functionality and updated tempBelow

--- Wills_UDS_ATC.ino
+++ Wills_UDS_ATC.ino
@@ -27,10 +27,21 @@ WiFiClient client;
 char auth[] = "<your Blynk key here>";
 
 
+//Define Variables we'll be connecting to (PID)
+double Setpoint, Input, Output;
+
+//Define the aggressive and conservative Tuning Parameters (PID)
+double aggKp=8, aggKi=0.4, aggKd=2;
+double consKp=4, consKi=0.2, consKd=1;
+
+//Specify the links and initial tuning parameters
+PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
+
+
 //initialize the fan controlling PWM pin and clock speed
 int fanControlpin = D0;
 int fanSpeed = 400; // value between 0-1020
-int minSpeed= 5;
+int minSpeed= 15;
 int PWMfreq=30000;
 
 bool E_StillCooking=true;  // when this gets set to false, the fan turns off completely and stays off
@@ -53,6 +64,7 @@ MAX6675 probe_D_Thermocouple(thermoSCKpin, probe_D_CSpin, thermoSOpin);
 MAX6675 probe_E_Thermocouple(thermoSCKpin, probe_E_CSpin, thermoSOpin);  
 int targetTemp=125;
 int pullTemp=95;
+int tempBelowTarget=30;
 float probe_A=25;
 float probe_B=25;
 float probe_C=25;
@@ -95,7 +107,9 @@ BLYNK_READ(V5){
 void setup() {                
   Serial.begin(9600);
   delay(10);
-  pinMode(fanControlpin,OUTPUT); //configure the fan controller 
+  
+ //configure the fan controller 
+  pinMode(fanControlpin,OUTPUT);
   analogWriteFreq(PWMfreq);
   analogWrite(fanControlpin, 300); // start the fan off
 
@@ -104,23 +118,29 @@ void setup() {
   Blynk.begin(auth, ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    Serial.print(".");   
   }
-  Serial.printf("\n\nWiFi connected\n\n");
-  timer.setInterval(20000, postToThingspeak);  // commented out until we want to re-enable internet connectivity
+    Serial.printf("\n\nWiFi connected\n\n");
+
+//PID
+  Input = tempWeightedAvgLast;
+  Setpoint = targetTemp;
+  myPID.SetMode(AUTOMATIC);  //turn the PID on
+//Start timers 
+  timer.setInterval(60000, postToThingspeak);  // commented out until we want to re-enable internet connectivity
   timer.setInterval(5000, readTempSensors);
   timer.setInterval(10000, fanController);
 }
   
 void loop() {
   if (E_StillCooking){
-  timer.run();
-  Blynk.run();
-   
+    timer.run();
+    Blynk.run();
     delay(500); 
-    
   } else {
     delay(60000);
+    timer.run();
+    Blynk.run();
   } 
 }
 
@@ -140,7 +160,9 @@ void postToThingspeak(){
            postStr +="&field6=";
            postStr += String(targetTemp);
            postStr +="&field7=";
-           postStr += String(pullTemp);
+           postStr += String(pullTemp); 
+           postStr +="&field8=";
+           postStr += String(tempBelowTarget);
            postStr += "\r\n\r\n"; 
      client.print("POST /update HTTP/1.1\n"); 
      client.print("Host: api.thingspeak.com\n"); 
@@ -158,24 +180,29 @@ void postToThingspeak(){
 }
 
 void fanController(){
-  if(probe_E_Last+1 <= pullTemp){  //check if the meat is almost done
-    int tempBelowTarget=targetTemp-tempWeightedAvgLast ;
-      if(tempBelowTarget > 0){
-        int newSpeed= 1000;
-        if (tempBelowTarget <= 19){
-           newSpeed=((sq(tempBelowTarget*0.5))+(0.2*tempBelowTarget)+15)*10;
+ if (tempBelowTarget > 1){  // check if the meat is almost done
+        Input = tempWeightedAvgLast;
+        Setpoint = targetTemp;
+        double gap = Setpoint-Input; // average temp distance away from target temperature
+        if(gap<=0){     
+            fanSpeed=minSpeed;
+            analogWrite(fanControlpin, fanSpeed);
+            Serial.println("Turned fan off. [Close to target temperature]");
+        } else if(gap > 0 && gap<=10) {  //we're close to setpoint, use conservative tuning parameters
+          myPID.SetTunings(consKp, consKi, consKd);
+          myPID.Compute();
+          fanSpeed=  map(Output, 0, 255, minSpeed, 1020);
+          analogWrite(fanControlpin, fanSpeed);
+          Serial.printf("** Turned fan on with speed = %d%.\n",(int)(fanSpeed/10.2));
+        } else {//we're far from setpoint, use aggressive tuning parameters
+           myPID.SetTunings(aggKp, aggKi, aggKd);
+           myPID.Compute();
+           fanSpeed=  map(Output, 0, 255, minSpeed, 1000);
+           analogWrite(fanControlpin, fanSpeed);
+           Serial.printf("** Turned fan on with speed = %d%.\n",(int)(fanSpeed/10));
         }
-         analogWrite(fanControlpin, newSpeed);
-         fanSpeed=newSpeed;
-         Serial.printf("** Turned fan on with speed = %d%.\n",(int)(fanSpeed/10));
-      }else if(fanSpeed > minSpeed){
-          fanSpeed=minSpeed;
-         analogWrite(fanControlpin, fanSpeed);
-         Serial.println("** Turned fan off.");
-      }else{
-         Serial.println("** Fan is off.");
-      }
-  }else{  // if the meat is at the pull temp, turn off fan, and notify that the food is done.
+      
+  }else {  // if the meat is at the pull temp, turn off fan, and notify that the food is done.
      fanSpeed=minSpeed;
      analogWrite(fanControlpin, fanSpeed);
      Serial.println("Turned fan off.");
@@ -183,7 +210,6 @@ void fanController(){
      Serial.println("#### Finished! Time to pull :-D  ####");
   }
 }
-
 
 void readTempSensors(){
    probe_A=probe_A_Thermocouple.readCelsius();
@@ -197,17 +223,9 @@ void readTempSensors(){
    probe_E=probe_E_Thermocouple.readCelsius(); 
 
    Serial.printf("pull\t|target\t|A\t|B\t|C\t|D\t|E\n%d\t|%d\t|%d\t|%d\t|%d\t|%d\t|%d\n",(int)pullTemp,(int)targetTemp,(int)probe_A_Last,(int)probe_B_Last,(int)probe_C_Last,(int)probe_D_Last,(int)probe_E_Last);
-   /*
- 
-   Serial.printf("Probe A temperature = %d\n",(int)probe_A_Last);
-   Serial.printf("Probe B temperature = %d\n",(int)probe_B_Last);
-   Serial.printf("Probe C temperature = %d\n",(int)probe_C_Last);
-   Serial.printf("Probe D temperature = %d\n",(int)probe_D_Last);
-   Serial.printf("Probe E temperature | pull temperature = %d | %d\n\n",(int)probe_E_Last,(int)pullTemp);
-   Serial.printf("Meat temperature = %d\n\n",(int)probe_E_Last);
-   */
+
    
-    tempWeightedAvg=(probe_A+probe_B+probe_C+probe_D)/4;
+    tempWeightedAvg=(probe_A+probe_B)/2;
 
     probe_A_Last=(2*probe_A_Last+probe_A)/3;
     probe_B_Last=(2*probe_B_Last+probe_B)/3;
@@ -215,6 +233,8 @@ void readTempSensors(){
     probe_D_Last=(2*probe_D_Last+probe_D)/3;
     probe_E_Last=(2*probe_E_Last+probe_E)/3;
     tempWeightedAvgLast=(2*tempWeightedAvgLast+tempWeightedAvg)/3;
+    tempBelowTarget=pullTemp-probe_E_Last ;
+
 }
 
 
